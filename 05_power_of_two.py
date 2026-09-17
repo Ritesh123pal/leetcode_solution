249d59e948bbbf9ec6680b8026965f5d60dab865
Ritesh Pal <palr44241@gmail.com>
2026-09-17 12:17:35 +0530

    Added power of two solution

---
+++ 05_power_of_two.py
@@ -0,0 +1,16 @@
+# Given an integer n, return true if it is a power of two. Otherwise, return false.
+# An integer n is a power of two, if there exists an integer x such that n == 2x.
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n>0:
+            if (n&(n-1))==0:
+                return True
+            else:
+                return False
+        else:
+           return False 
+        
