70bfeaf113cadf2fc296c22b56a75f05b2fe851e
Ritesh Pal <palr44241@gmail.com>
2026-09-20 18:49:36 +0530

    Added  sum of two integer solution

---
+++ 11_Add_two_integer.py
@@ -0,0 +1,9 @@
+# Given two integers num1 and num2, return the sum of the two integers.
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
