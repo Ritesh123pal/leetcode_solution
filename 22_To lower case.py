617f1a4450b183f9e0792efe8126f348749de3f2
Ritesh Pal <palr44241@gmail.com>
2026-09-26 20:19:14 +0530

    Added to lower case solution

---
+++ 22_To lower case.py
@@ -0,0 +1,9 @@
+# Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.lower()
+ 
